79aabf997ae0bb41e4f5948b1ab29538c8751990
Dylan King <xylvnking@gmail.com>
2022-12-05 08:31:33 -0500

    init unreal project with lfs

---
+++ Source/ArenaCombat/ArenaCombat.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArenaCombat.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaCombat, "ArenaCombat" );
+ 
